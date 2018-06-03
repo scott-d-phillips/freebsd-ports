6f31c72b3662adbcf8330fd128b2f08f4f8964a6
Scott D Phillips <scott.d.phillips@intel.com>
2018-06-03 06:32:50 -0700

    graphics: add sway port
    sway is "SirCmpwn's Wayland compositor", a work-in-progress
    i3-compatible Wayland compositor.

---
+++ graphics/sway/files/patch-sway_commands.c
@@ -0,0 +1,10 @@
+--- sway/commands.c.orig	2018-05-13 18:18:07 UTC
++++ sway/commands.c
+@@ -1,6 +1,6 @@
+-#define _XOPEN_SOURCE 500
+ #include <ctype.h>
+ #include <stdarg.h>
++#include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <strings.h>
---
+++ graphics/sway/files/patch-swaybar_ipc.c
@@ -0,0 +1,9 @@
+--- swaybar/ipc.c.orig	2018-05-13 18:18:07 UTC
++++ swaybar/ipc.c
+@@ -1,5 +1,5 @@
+-#define _XOPEN_SOURCE 500
+ #include <limits.h>
++#include <stdio.h>
+ #include <string.h>
+ #include <strings.h>
+ #include <json-c/json.h>
